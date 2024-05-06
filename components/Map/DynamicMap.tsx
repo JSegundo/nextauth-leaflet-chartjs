9f9b3c0a3f069a8a6d54bcc64a43d2d99ec107a6
segundo <segujuan27@hotmail.com>
2024-05-06 16:12:58 +0200

    fixed leaflet auth in prod

--- components/Map/DynamicMap.tsx
+++ components/Map/DynamicMap.tsx
@@ -33,11 +33,11 @@ const Map = ({ zoom, center }: MapProps) => {
       >
         <TileLayer
           // sin nombes capaz mejor calidad url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-          url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.jpg"
+          // url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.jpg"
           // attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
 
-          // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          // attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         <GeocoderControl />
 
